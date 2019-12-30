dc0d69aaa81268ee8a776713a478c7f1fe8aa16a
Sohit <sumitk002@gmail.com>
2019-12-30 16:21:23 +0530

    added merged state for PR

--- domain/factory/pull_request_factory.py
+++ domain/factory/pull_request_factory.py
@@ -38,7 +38,6 @@ class GitHubPullRequestFactory(EventFactory):
         if action == "closed" and event["pull_request"]["merged"]:
             action == "merged"
 
-
         pull_request = PullRequest(repository_url, pull_request_id, commits, source_branch, target_branch, title,
                                    reviewers, sender_name, sender_id, action, commits_url, merged_at, created_at,
                                    updated_at, closed_at, merge_commit_sha, review_comments, no_of_commits,
